f93605cdb2c486a9539a5161a69ec3c0fdcfa96e
the1glorfindel <glorfindelofrivendell@outlook.com>
2015-10-23 13:57:06 -0500

    Create sickle.lua

---
+++ sickle.lua
@@ -0,0 +1,41 @@
+farming.register_sickle(":farming:sickle_wood", {
+	description = "Wooden Sickle",
+	inventory_image = "farming_tool_woodhoe.png",
+	max_uses = 60,
+	material = "group:wood"
+})
+
+farming.register_sickle(":farming:sickle_stone", {
+	description = "Stone Sickle",
+	inventory_image = "farming_tool_stonehoe.png",
+	max_uses = 120,
+	material = "group:stone"
+})
+
+farming.register_sickle(":farming:sickle_steel", {
+	description = "Steel Sickle",
+	inventory_image = "farming_tool_steelhoe.png",
+	max_uses = 300,
+	material = "default:steel_ingot"
+})
+
+farming.register_sickle(":farming:sickle_bronze", {
+	description = "Bronze Sickle",
+	inventory_image = "farming_tool_bronzehoe.png",
+	max_uses = 220,
+	material = "default:bronze_ingot"
+})
+
+farming.register_sickle(":farming:sickle_mese", {
+	description = "Mese Sickle",
+	inventory_image = "farming_tool_mesehoe.png",
+	max_uses = 450,
+	material = "default:mese_crystal"
+})
+
+farming.register_sickle(":farming:sickle_diamond", {
+	description = "Diamond Sickle",
+	inventory_image = "farming_tool_diamondhoe.png",
+	max_uses = 1000,
+	material = "default:diamond"
+})
